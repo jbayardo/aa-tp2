Fix q average calculation for great speed gains

src/impl/four_row_state.py
@@ -1,7 +1,6 @@
 import copy
 from abstract.state import State
 import numpy as np
-import hashlib
 
 _rows = 6
 _columns = 7
@@ -14,7 +13,8 @@ _size = _rows * _columns
 # Ensure the actual board size is lower than the maximum than can be held by an uint64
 assert (_rows1 * _columns) < 64
 
-# Generate the bitmask for checking if a row has been filled
+# Generate the bitmask for checking if the entire board has been filled
+# taken from https://github.com/nwestbury/pyConnect4/blob/master/board.py
 _full_board = np.uint64(0)
 for column in range(_columns):
     for row in range(_rows):
@@ -101,17 +101,15 @@ class FourRowState(State):
 
     @property
     def is_draw(self):
-        # taken from https://github.com/nwestbury/pyConnect4/blob/master/board.py
         return self.board == _full_board
 
     @property
     def is_terminal(self) -> bool:
         return self.is_draw or _is_winning_board(self._boards[0]) or _is_winning_board(self._boards[1])
 
-    # TODO: revisit
     # The following are required for dictionary usage
     def __hash__(self):
-        return hash(self.board)
+        return int(self.board)
 
     def __eq__(self, other: 'FourRowState') -> bool:
         return self.board == other.board

src/learning_mechanism.py
@@ -2,6 +2,7 @@ from abstract.agent import Agent
 from impl.four_row_agent import FourRowAgent
 from impl.four_row_state import FourRowState
 import random
+import numpy as np
 
 
 class LearningMatch(object):
@@ -62,16 +63,15 @@ class LearningMatch(object):
             winner, turns = self.train_single_match(learning_rate, discount_factor)
 
             # Compute average value for the Q
-            avgq = 0.0
-            totq = 0.0
-            for key in self.learning_player._q_definition:
-                avgq += self.learning_player._q_definition[key]
-                totq += 1.0
+            vals = np.array(list(self.learning_player._q_definition.values()), dtype=np.float64)
+            avgq = np.mean(vals)
+            devq = np.std(vals)
 
             samples.append({
                 'episode_number': episode,
                 'winner': winner,
-                'avg_q': avgq
+                'avg_q': avgq,
+                'std_q': devq
             })
 
         return self.learning_player, samples

src/q_agent.py
@@ -1,7 +1,7 @@
 from abstract.agent import Agent
 from abstract.state import State
 import collections
-import random
+import numpy as np
 
 
 class QAgent(Agent):
@@ -59,7 +59,7 @@ class QAgent(Agent):
                        current_q + learning_rate * (reward + discount_factor * maximum_factor - current_q))
 
     def _q_initialize(self):
-        self._q_definition = collections.defaultdict(random.random)
+        self._q_definition = collections.defaultdict(lambda: np.random.uniform(0.5, 1.0))
 
     def _q_update(self, state: State, action, value: float):
         self._q_definition[(state, action)] = value